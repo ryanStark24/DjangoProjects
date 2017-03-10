baceb8ec37f7a03b267473dd614bd704fefe0989
ryanStark24 <anshul2411.rs@gmail.com>
2017-03-10 08:51:17 +0530

    base.js updated,link change highlight problem solved

--- dj3/college/static/base.js
+++ dj3/college/static/base.js
@@ -1,4 +1,13 @@
-$('.navbar-nav li').click( function () {
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-    });
+$(document).ready(function() {
+
+	$(location).attr('href');
+
+	if (window.location.pathname.split('/')[2]) {
+		var attr = '#' + window.location.pathname.split('/')[2];
+
+		$(attr).addClass('active');
+	} else {
+		$('#home').addClass('active');
+	}
+
+});
